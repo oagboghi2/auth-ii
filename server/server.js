f3f1843e833519ab33c2c2bddd93a929c6c8b895
Obo Agboghidi <oagboghi2@gmail.com>
2018-06-13 16:15:42 -0700

    Obo Agboghidi: Working on getting post routes to work

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
 
 const server = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // allow only the React application to connect
+  credentials: true, // sets the Access-Control-Allow-Credentials CORS header
+};
+
+server.use(express.json());
+server.use(cors(corsOptions));
+
+
 
 setupMiddleware(server);
 setupRoutes(server);
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('./User');
 
-router.get('/', (req, res) => {
+function generateToken(user){
+  const options = {
+    expiresIn: '1h',
+  }
+  //sign the token
+const secret = 'Secret is yours';
+const payload = { name: user.username, }
+return jwt.sign(payload, secret, options)
+}
+
+router.get('/api', (req, res) => {
   User.find()
     .select('-password')
     .then(users => {
@@ -12,5 +23,60 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router
+  .post('/api/signup', (req, res) => {
+    User.create(req.body)
+      .then(user => {
+        const token = generateToken(user)
+
+        res.status(201).json({ username: user.username, token })
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  })
+
+router
+  .post('/api/signin', (req, res)=>{
+    const { username, password } = req.body;
+
+    User.findOne({ username, password})
+    .then(user => {
+      if(user){
+        user
+          .validatePassword(password)
+          .then(passwordsMatch => {
+            if(passwordsMatch){
+              //generate the token
+              const token = generateToken(user);
+              //send token back to the client
+              res.status(200).json({ message: `welcome ${username}!`, token });
+            } else {
+              res.status(401).json({ errorMessage: 'invalid credentials'});
+            }
+          })
+          .catch(err => {
+            res.send('error comparing passwords');
+          }); 
+      } else {
+        res.status(401).send('invalid credentials')
+      } 
+    })
+    .catch(err => {
+      res.send(err);
+    });
+  });
+
+router
+  .post('/api/signup', (req, res)=>{
+    User.create(req.body)
+      .then(user => {
+        const token = generateToken(user);
+
+        res.status(201).json({ username: user.username, token });
+      })
+      .catch(er => res.status(500).json(err));
+  })
 
 module.exports = router;
